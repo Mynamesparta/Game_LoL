f0cb93d72da5cae878ff3c1a79e04148c0e0f61f
Mynamesparta <Mynamesparta@i.ua>
2014-12-06 20:30:48 +0200

    add class Symmetric

--- Client_Qt/Client/Encryption/symmetric.cpp
+++ Client_Qt/Client/Encryption/symmetric.cpp
@@ -23,21 +23,41 @@ QString Symmetric::Encryption(const QString& text)
         result.push_back((int)text[i].toLatin1());
         if(_private_key<=result<<1)
         {
-
+            result*=_private_key;
             encryption_text+= result.toString()+(i+1==text.length()?"":"|");
             result.clear();
         }
     }
     if(_private_key>(result<<1))
     {
+        result*=_private_key;
         encryption_text+= result.toString();
     }
+    qDebug()<<qS<<"after encryption"<<encryption_text;
     return encryption_text;
 }
 
 QString Symmetric::Decipherment(const QString& encryption_text)
 {
-    return encryption_text;
+    QStringList qslist=encryption_text.split("|");
+    LongInt result;
+    qDebug()<<qS<<"before decipherment"<<encryption_text;
+    QString dicipherment_text;
+    for(int i=0;i<qslist.length();i++)
+    {
+        result=LongInt(qslist[i]);
+        result/=_private_key;
+        dicipherment_text+=result.toString()+(i+1==qslist.length()?"":"");
+    }
+    qDebug()<<qS<<"after decipherment"<<dicipherment_text;
+    qslist=dicipherment_text.split(" ");
+    dicipherment_text.clear();
+    for(int i=0;i<qslist.length();i++)
+    {
+        dicipherment_text+=(char)qslist[i].toInt();
+        qDebug()<<qS<<dicipherment_text;
+    }
+    return dicipherment_text;
 }
 
 const LongInt& Symmetric::private_key()
